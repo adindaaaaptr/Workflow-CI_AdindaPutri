d3c2a2eaa125a3bf2246fb9fa7634cbe1bc4feae
adindaaaaptr <adindapnr2005@gmail.com>
2026-06-01 00:19:58 +0700

    test

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,41 +1,35 @@
 import pandas as pd
-import mlflow
-import mlflow.sklearn
-
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("iris-experiment")
-mlflow.sklearn.autolog()
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+import os
 
-df = pd.read_csv("Iris.csv")
+# Baca data hasil preprocessing
+train_df = pd.read_csv("iris_preprocessing/iris_train_preprocessed.csv")
+test_df = pd.read_csv("iris_preprocessing/iris_test_preprocessed.csv")
 
-if "Id" in df.columns:
-    df = df.drop(columns=["Id"])
+X_train = train_df.drop("Species", axis=1)
+y_train = train_df["Species"]
+X_test = test_df.drop("Species", axis=1)
+y_test = test_df["Species"]
 
-X = df.drop("Species", axis=1)
-y = df["Species"]
+# Latih model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-le = LabelEncoder()
-y = le.fit_transform(y)
+# Evaluasi
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {acc}")
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+# Simpan model ke folder 'model' (format MLflow)
+os.makedirs("model", exist_ok=True)
+mlflow.sklearn.save_model(model, "model")
 
+# (Opsional) logging ke MLflow lokal
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("iris-ci")
 with mlflow.start_run():
-
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-   
     mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    print("Accuracy:", acc)
+    mlflow.sklearn.log_model(model, "model_artifacts")
